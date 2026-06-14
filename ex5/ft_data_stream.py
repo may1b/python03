5a385a2a6ca01a37c66c41b63bde64b26c64f9a6
May <magrass@student.42heilbronn.de>
2026-06-14 14:07:34 +0200

    done ex5!

---
+++ ex5/ft_data_stream.py
@@ -0,0 +1,43 @@
+import random
+from typing import Generator
+
+PLAYERS = ['alice', 'bob', 'charlie', 'dylan']
+ACTIONS = ['run', 'eat', 'sleep', 'grab', 'move',
+           'climb', 'swim', 'release', 'use']
+
+
+def gen_event() -> Generator[tuple[str, str], None, None]:
+    while True:
+        yield (random.choice(PLAYERS), random.choice(ACTIONS))
+
+
+def consume_event(
+    events: list[tuple[str, str]],
+) -> Generator[tuple[str, str], None, None]:
+    while len(events) > 0:
+        idx = random.randint(0, len(events) - 1)
+        event = events[idx]
+        del events[idx]
+        yield event
+
+
+def main() -> None:
+    print("=== Game Data Stream Processor ===")
+
+    stream = gen_event()
+    for i in range(1000):
+        name, action = next(stream)
+        print(f"Event {i}: Player {name} did action {action}")
+
+    events: list[tuple[str, str]] = []
+    for _ in range(10):
+        events.append(next(stream))
+    print(f"Built list of 10 events: {events}")
+
+    for event in consume_event(events):
+        print(f"Got event from list: {event}")
+        print(f"Remains in list: {events}")
+
+
+if __name__ == "__main__":
+    main()
